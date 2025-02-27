e03fef7f591aecb2fca50962ed22be96f3d8a570
Jordan Moureau <jordanfromnovember@gmail.com>
2025-02-27 09:59:24 -0800

    logic for mobile responsive portfolio images

--- src/components/ScrollGallery.js
+++ src/components/ScrollGallery.js
@@ -1,12 +1,35 @@
 "use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const images = ["/one.png", "/two.png", "/three.png", "/four.png"];
+const imagesmobile = [
+  "/mobileone.png",
+  "/mobiletwo.png",
+  "/mobilethree.png",
+  "/mobilefour.png",
+];
 
 export default function SnapScrollGallery() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const imageSet = isMobile ? imagesmobile : images;
+
   return (
     <div className="h-screen w-full">
-      {images.map((src, index) => (
+      {imageSet.map((src, index) => (
         <div
           key={index}
           className="h-screen w-full flex items-center justify-center snap-start"
